bccbd38b6c377fe20b3c0623557f73e989eb855a
out2250 <out2250@fifgroup.co.id>
2026-03-06 13:35:54 +0700

    fixing

--- gorga-kasir-be/prisma/migrations/20260305091500_stock_opname_assignment/migration.sql
+++ gorga-kasir-be/prisma/migrations/20260305091500_stock_opname_assignment/migration.sql
@@ -1,8 +1,8 @@
-ALTER TABLE `stockopnamesession`
+ALTER TABLE `StockOpnameSession`
   ADD COLUMN `assignedTo` VARCHAR(191) NULL,
   ADD COLUMN `assignedBy` VARCHAR(191) NULL,
   ADD COLUMN `assignedAt` DATETIME(3) NULL,
   ADD CONSTRAINT `stockopnamesession_assignedTo_fkey`
-    FOREIGN KEY (`assignedTo`) REFERENCES `user`(`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY (`assignedTo`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE,
   ADD CONSTRAINT `stockopnamesession_assignedBy_fkey`
-    FOREIGN KEY (`assignedBy`) REFERENCES `user`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+    FOREIGN KEY (`assignedBy`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
